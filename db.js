8d0aa8b7dc59bf3a725c059f104552a33bf53bbe
student <student@eda-bc-09.localdomain>
2021-01-22 10:59:15 +1300

    profile page and im feeling indecisive routes completed

--- db.js
+++ db.js
@@ -4,7 +4,13 @@ const connection = require('knex')(config)
 
 module.exports = {
   getUser: getUser,
-  getUsers: getUsers
+  getUsers: getUsers,
+  addUser: addUser,
+  addFavBird: addFavBird,
+  addFavCharacter: addFavCharacter,
+  addFavCountry: addFavCountry,
+  addFavSuperPower: addFavSuperPower,
+  getAllFromUser: getAllFromUser
 }
 
 function getUsers (db = connection) {
@@ -13,4 +19,39 @@ function getUsers (db = connection) {
 
 function getUser (id, db = connection) {
   return db('users').where('id', id).first()
+}
+
+function addUser(name, db = connection) {
+  return db("users")
+  .insert({name: name})
+}
+
+function addFavBird(bird, userid, db = connection) {
+  return db("fav_bird")
+  .insert({bird: bird, user_id: userid})
+}
+
+function addFavCharacter(character,userid, db = connection) {
+  return db("fav_character")
+  .insert({character: character, user_id: userid})
+}
+
+function addFavCountry(country,userid, db = connection) {
+  return db("fav_countries")
+  .insert({country: country, user_id: userid})
+}
+
+function addFavSuperPower(superpower,userid, db = connection) {
+  return db("superpower")
+  .insert({superpower: superpower, user_id: userid})
+}
+
+function getAllFromUser(id, db = connection) {
+  return db("users")
+  .join('fav_bird', "users.id", "fav_bird.user_id" )
+  .join("fav_character","users.id", "fav_character.user_id")
+  .join("fav_countries","users.id", "fav_countries.user_id")
+  .join("superpower","users.id", "superpower.user_id")
+  .where("userid", id)
+  .select("*", "users.id AS userid")
 }
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,56 @@
-const express = require('express')
+const express = require('express');
 
-const db = require('../db')
+const db = require('../db');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', (req, res) => {
-  db.getUsers()
-    .then(users => {
-      res.render('index', {users: users})
-    })
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
-})
+	db.getUsers()
+		.then((users) => {
+			res.render('index', { users: users });
+		})
+		.catch((err) => {
+			res.status(500).send('DATABASE ERROR: ' + err.message);
+		});
+});
 
+router.get('/imFeelingIndecisive', (req, res) => {
+	res.render('imFeelingIndecisive', {});
+});
 
+router.post('/imFeelingIndecisive', (req, res) => {
+	let user = {
+		name: req.body.name,
+		fav_bird: req.body.fav_bird,
+		fav_character: req.body.fav_character,
+		fav_countries: req.body.fav_countries,
+		superpower: req.body.superpower,
+	};
 
-module.exports = router
+	db.addUser(user.name).then((result1) => {
+    db.addFavBird(user.fav_bird, result1[0])
+    .then(() => {
+      db.addFavCharacter(user.fav_character, result1[0])
+      .then(() => {
+        db.addFavCountry(user.fav_countries, result1[0])
+        .then(() => {
+          db.addFavSuperPower(user.superpower, result1[0])
+          .then(() => {});
+          console.log(result1[0])
+          res.redirect('/profilePage/' + result1[0]);
+				});
+			});
+		});
+	});
+});
+
+router.get('/profilePage/:id', (req, res) => {
+	let id = req.params.id;
+
+  db.getAllFromUser(id)
+  .then((user) => {
+		res.render('profilePage', user);
+	});
+});
+
+module.exports = router;
