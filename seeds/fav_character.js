c86e3d4da22e3b27093a0f1b38c2342bff35d255
student <student@eda-bc-11.localdomain>
2021-01-22 09:50:47 +1300

    Add favorite character seed

--- seeds/fav_character.js
+++ seeds/fav_character.js
@@ -1,13 +1,20 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('favCharacter').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('favCharacter').insert([
+        { user_id: 99901, country: 'Mickey Mouse' },
+				{ user_id: 99902, country: 'Mickey Mouse' },
+				{ user_id: 99903, country: 'Mickey Mouse' },
+				{ user_id: 99904, country: 'Toothless' },
+				{ user_id: 99905, country: 'Toothless' },
+				{ user_id: 99906, country: 'Toothless' },
+				{ user_id: 99907, country: 'Cinderella' },
+				{ user_id: 99908, country: 'Cinderella' },
+				{ user_id: 99909, country: 'Cinderella' },
+				{ user_id: 99910, country: 'Bart Simpson'}
       ]);
     });
 };
