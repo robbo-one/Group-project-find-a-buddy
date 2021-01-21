d3aba7fd5e1bfda5f1fe51b5cc95d1fd2d3acf6c
student <student@eda-bc-09.localdomain>
2021-01-22 09:33:34 +1300

    country seed completed

--- seeds/fav_countries.js
+++ seeds/fav_countries.js
@@ -5,16 +5,16 @@ exports.seed = function (knex) {
 		.then(function () {
 			// Inserts seed entries
 			return knex('table_name').insert([
-				{ user_id: 99901, country: 'Ambitious Aardvark' },
-				{ user_id: 99902, country: 'Bamboozled Baboon' },
-				{ user_id: 99903, country: 'Curious Capybara' },
-				{ user_id: 99904, country: 'Dilapidated Duck' },
-				{ user_id: 99905, country: 'Exuberant Elephant' },
-				{ user_id: 99906, country: 'Fascinated Flying Fox' },
-				{ user_id: 99907, country: 'Generous Gila Monster' },
-				{ user_id: 99908, country: 'Hilarious Heron' },
-				{ user_id: 99909, country: 'Intransigent Impala' },
-				{ user_id: 99910, country: 'Jocular Jerboa' },
+				{ user_id: 99901, country: 'New Zealand' },
+				{ user_id: 99902, country: 'New Zealand' },
+				{ user_id: 99903, country: 'New Zealand' },
+				{ user_id: 99904, country: 'Japan' },
+				{ user_id: 99905, country: 'Japan' },
+				{ user_id: 99906, country: 'Japan' },
+				{ user_id: 99907, country: 'Australia' },
+				{ user_id: 99908, country: 'Australia' },
+				{ user_id: 99909, country: 'Australia' },
+				{ user_id: 99910, country: 'U.S.A' },
 			]);
 		});
 };
