e1725253011c80a16990ee0062ad2e172749c159
DrSaac <44042755+DrSaac@users.noreply.github.com>
2021-01-21 21:49:21 +1300

    added main and layout

--- migrations/20160802144759_users.js
+++ migrations/20160802144759_users.js
@@ -2,7 +2,6 @@ exports.up = (knex, Promise) => {
   return knex.schema.createTable('users', (table) => {
     table.increments('id').primary()
     table.string('name')
-    table.string('email')
   })
 }
 
