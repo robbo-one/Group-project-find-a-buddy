68b192b97b678a17c610919c6b4c8caaf4e7255e
student <student@eda-bc-09.localdomain>
2021-01-22 09:51:53 +1300

    fav_birds seed completed

--- seeds/fav_bird.js
+++ seeds/fav_bird.js
@@ -1,13 +1,20 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('fav_birds').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('fav_birds').insert([
+        { user_id: 99901, fav_birds: 'Kea' },
+				{ user_id: 99902, fav_birds: 'Kea' },
+				{ user_id: 99903, fav_birds: 'Kea' },
+				{ user_id: 99904, fav_birds: 'Piwakawaka' },
+				{ user_id: 99905, fav_birds: 'Piwakawaka' },
+				{ user_id: 99906, fav_birds: 'Piwakawaka' },
+				{ user_id: 99907, fav_birds: 'Kiwi' },
+				{ user_id: 99908, fav_birds: 'Kiwi' },
+				{ user_id: 99909, fav_birds: 'Kiwi' },
+				{ user_id: 99910, fav_birds: 'Ostrich' },
       ]);
     });
 };
