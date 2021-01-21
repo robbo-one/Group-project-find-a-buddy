6e3e4f518d3fc951f43f57e1dd376b3c188b117c
student <student@eda-bc-11.localdomain>
2021-01-22 09:58:38 +1300

    Change favorite character seed

--- seeds/fav_character.js
+++ seeds/fav_character.js
@@ -1,20 +1,20 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('favCharacter').del()
+  return knex('fav_character').del()
     .then(function () {
       // Inserts seed entries
-      return knex('favCharacter').insert([
-        { user_id: 99901, country: 'Mickey Mouse' },
-				{ user_id: 99902, country: 'Mickey Mouse' },
-				{ user_id: 99903, country: 'Mickey Mouse' },
-				{ user_id: 99904, country: 'Toothless' },
-				{ user_id: 99905, country: 'Toothless' },
-				{ user_id: 99906, country: 'Toothless' },
-				{ user_id: 99907, country: 'Cinderella' },
-				{ user_id: 99908, country: 'Cinderella' },
-				{ user_id: 99909, country: 'Cinderella' },
-				{ user_id: 99910, country: 'Bart Simpson'}
+      return knex('fav_character').insert([
+        { user_id: 99901, character: 'Mickey Mouse' },
+				{ user_id: 99902, character: 'Mickey Mouse' },
+				{ user_id: 99903, character: 'Mickey Mouse' },
+				{ user_id: 99904, character: 'Toothless' },
+				{ user_id: 99905, character: 'Toothless' },
+				{ user_id: 99906, character: 'Toothless' },
+				{ user_id: 99907, character: 'Cinderella' },
+				{ user_id: 99908, character: 'Cinderella' },
+				{ user_id: 99909, character: 'Cinderella' },
+				{ user_id: 99910, character: 'Bart Simpson'}
       ]);
     });
 };
