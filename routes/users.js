0bb852d495fee5f376c8a98ca5332bc64aca460b
student <student@eda-bc-09.localdomain>
2021-01-22 11:46:44 +1300

    main branch oops

--- routes/users.js
+++ routes/users.js
@@ -47,7 +47,8 @@ router.post('/imFeelingIndecisive', (req, res) => {
 });
 
 router.get('/profilePage/:id', (req, res) => {
-	let id = req.params.id;
+  let id = req.params.id;
+  
 
   db.getAllFromUser(id)
   .then((user) => {
@@ -55,5 +56,35 @@ router.get('/profilePage/:id', (req, res) => {
 		res.render('profilePage', {user: user});
 	});
 });
+
+router.get('/makeyourchoice', (req, res) => {
+res.render('makeyourchoice', {})
+})
+
+router.post('/makeyourchoice', (req, res) => {
+  let user = {
+		name: req.body.name,
+		fav_bird: req.body.fav_bird,
+		fav_character: req.body.fav_character,
+		fav_countries: req.body.fav_countries,
+		superpower: req.body.superpower,
+  };
+  db.addUser(user.name).then((result1) => {
+    db.addFavBird(user.fav_bird, result1[0])
+    .then(() => {
+      db.addFavCharacter(user.fav_character, result1[0])
+      .then(() => {
+        db.addFavCountry(user.fav_countries, result1[0])
+        .then(() => {
+          db.addFavSuperPower(user.superpower, result1[0])
+          .then(() => {});
+          console.log(result1[0])
+          res.redirect('/profilePage/' + result1[0]);
+				});
+			});
+		});
+	});
+});
+
 
 module.exports = router;
