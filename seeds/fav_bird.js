a5d002baeb3170cc378c08629216cc56829e9449
student <student@eda-bc-09.localdomain>
2021-01-22 10:09:43 +1300

    home page loads

--- seeds/fav_bird.js
+++ seeds/fav_bird.js
@@ -1,20 +1,20 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('fav_birds').del()
+  return knex('fav_bird').del()
     .then(function () {
       // Inserts seed entries
-      return knex('fav_birds').insert([
-        { user_id: 99901, fav_birds: 'Kea' },
-				{ user_id: 99902, fav_birds: 'Kea' },
-				{ user_id: 99903, fav_birds: 'Kea' },
-				{ user_id: 99904, fav_birds: 'Piwakawaka' },
-				{ user_id: 99905, fav_birds: 'Piwakawaka' },
-				{ user_id: 99906, fav_birds: 'Piwakawaka' },
-				{ user_id: 99907, fav_birds: 'Kiwi' },
-				{ user_id: 99908, fav_birds: 'Kiwi' },
-				{ user_id: 99909, fav_birds: 'Kiwi' },
-				{ user_id: 99910, fav_birds: 'Ostrich' },
+      return knex('fav_bird').insert([
+        { user_id: 99901, bird: 'Kea' },
+				{ user_id: 99902, bird: 'Kea' },
+				{ user_id: 99903, bird: 'Kea' },
+				{ user_id: 99904, bird: 'Piwakawaka' },
+				{ user_id: 99905, bird: 'Piwakawaka' },
+				{ user_id: 99906, bird: 'Piwakawaka' },
+				{ user_id: 99907, bird: 'Kiwi' },
+				{ user_id: 99908, bird: 'Kiwi' },
+				{ user_id: 99909, bird: 'Kiwi' },
+				{ user_id: 99910, bird: 'Ostrich' },
       ]);
     });
 };
